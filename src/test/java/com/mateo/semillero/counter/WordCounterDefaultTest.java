a49bc550aff74c53112a3a49e43c626e8ba40464
Mateo Ospina <mospinaa@devco.com.co>
2022-09-14 11:03:01 -0500

    Se crea la clase de pruebas unitarias para la clase WordCounter

---
+++ src/test/java/com/mateo/semillero/counter/WordCounterDefaultTest.java
@@ -0,0 +1,12 @@
+package com.mateo.semillero.counter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class WordCounterDefaultTest {
+
+    @Test
+    void mustReturnCorrectSummary() {
+        Assertions.fail();
+    }
+}
